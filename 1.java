8de36747ecfdcbe312cc992a9c4f0d1d86e768e0
Aditya Ramkumar <ramkumar.aditya@gmail.com>
2018-11-19 18:22:21 -0800

    Added solution

--- 1.java
+++ 1.java
@@ -7,9 +7,8 @@ class Solution {
     public int[] twoSum(int[] nums, int target) {
         HashMap<Integer, Integer> saved = new HashMap<>();
         for(int i = 0; i < nums.length; i++) {
-            if(saved.containsKey(nums[i])) {
+            if(saved.containsKey(nums[i]))
                 return new int[]{saved.get(nums[i]), i};
-            }
             saved.put(target - nums[i], i);
         }
         return new int[]{-1, -1};
